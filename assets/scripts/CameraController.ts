0c1087762079df632429522abe5ab095a50f56f2
Lucas Benvenuti <lucasbenvenuti2010@gmail.com>
2021-07-20 01:06:34 -0300

    Added 6 planets, rotation fixes and rotation X limit

--- assets/scripts/CameraController.ts
+++ assets/scripts/CameraController.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, math, systemEvent, SystemEvent, macro, game, cclegacy, Touch, EventKeyboard, EventMouse, Node, Vec3, tween, quat, Quat, AnimationComponent } from "cc";
+import { _decorator, Component, math, systemEvent, SystemEvent, macro, game, cclegacy, Touch, EventKeyboard, EventMouse, Node, Vec3, tween, quat, Quat, AnimationComponent, clamp } from "cc";
 import { Planet_Behavior } from "./Planet_Behavior";
 const { ccclass, property } = _decorator;
 
@@ -22,6 +22,12 @@ export class CameraController extends Component {
 
 	@property
 	rotateSpeed = 1;
+
+    @property({ slide: true, range: [0, 70, 0.01] })
+    limitUp_angle = 0;
+
+    @property({ slide: true, range: [-70, 0, 0.01] })
+    limitDown_angle = 0;
 
 	_euler = new math.Vec3();
 	_velocity = new math.Vec3();
@@ -65,12 +71,18 @@ export class CameraController extends Component {
 
 	update(dt: number) {
         var self = this;
+
         if(!self.hasSelectedPlanet)
         {
             // rotation
+            // let clampedAngle = clamp(self._euler.x, self.limitDown_angle, self.limitUp_angle);
+
             math.Quat.fromEuler(qt_1, self._euler.x, self._euler.y, self._euler.z);
             math.Quat.slerp(qt_1, self.node.rotation, qt_1, dt / self.damp);
-            self.node.setRotation(qt_1);
+            
+            // self.node.setRotationFromEuler
+
+            self.node.setRotation(qt_1.x, qt_1.y, qt_1.z, qt_1.w);
         }
 	}
 
@@ -88,9 +100,11 @@ export class CameraController extends Component {
         e.getDelta(v2_2);
 
         if(!self.hasSelectedPlanet)
-        {   
+        {
             this._euler.y -= v2_2.x * self.rotateSpeed;
-            this._euler.x += v2_2.y * self.rotateSpeed;
+
+            let clampedAngle = clamp(self._euler.x + (v2_2.y * self.rotateSpeed), self.limitDown_angle, self.limitUp_angle);
+            this._euler.x = clampedAngle;
         }
 
         if(self.currentPlanetBehavior && self.canInteractWithPlanet)
@@ -118,7 +132,7 @@ export class CameraController extends Component {
     
             self.translateCameraToCustomValue(self.currentPlanetBehavior.cameraPoint.getWorldPosition());
     
-            self.scheduleOnce(()=> {
+                self.scheduleOnce(()=> {
                 self.rotateCameraToCustomValue(self.currentPlanetBehavior.cameraPoint.getRotation());
             }, 1);
     
@@ -144,16 +158,44 @@ export class CameraController extends Component {
         }).start();
     }
 
+    easeInOutCubic(t: number) {
+        return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1;
+    }
+
     rotateCameraToCustomValue(newRotValue: Quat)
     {
+        const tw = tween(this.node);// Use tween animation
+        const quat_start = new Quat();
+
+        this.node.getRotation(quat_start);// Get the starting quaternion
+        var quat_end = new Quat(); // The final rotating quaternion assumes that it has been calculated
+        const quat_now = new Quat();// Use an intermediate variable
+
+        quat_end = newRotValue;
+        
+        tw.to(1.6, {}, {
+            easing: 'cubicInOut',
+            onUpdate: (target, ratio: number) => {
+                
+                // console.log(ratio);
+                // console.log();
+
+                // ratio : 0~1
+        // The spherical interpolation is used here, so there will be no deformation when rotating
+                quat_now.set(quat_start).slerp(quat_end, self.easeInOutCubic(ratio));
+                this.node.setRotation(quat_now);
+            },
+        })
+        tw.start();
+
         var self = this;
 
-        tween(self.node.getRotation()).to(1.6, new Quat(newRotValue), {
-            easing: 'cubicInOut',
-            'onUpdate': (currentValue: Quat) => {
-                self.node.setRotation(currentValue);
-            }
-        }).start();
+        // tween(self.node).to(1.6, {}, {
+        //     easing: 'cubicInOut',
+        //     'onUpdate': (currentValue: Quat) => {
+        //         self.node.setRotation(currentValue);
+        //     }
+        // }).start();
     }
 
     returnToStartPosition() {
